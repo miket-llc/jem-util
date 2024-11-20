bb9115418bad55e6f51a2f8468da7a218ed42a7f
miket-llc <mdt_@msn.com>
2024-11-20 14:36:00 -0500

    docs

--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,17 @@
-// jem-util/src/logger.ts
 import { createLogger, format, transports, Logger } from 'winston'
 import * as path from 'path'
 import { appName, appVersion } from './versions'
 
+/**
+ * Destructures and imports specific functions from the `format` module.
+ *
+ * @constant
+ * @type {Object}
+ * @property {Function} combine - Combines multiple format functions into a single format function.
+ * @property {Function} timestamp - Adds a timestamp to the log message.
+ * @property {Function} label - Adds a label to the log message.
+ * @property {Function} printf - Creates a custom log message format.
+ */
 const { combine, timestamp, label, printf } = format
 
 // Custom format for log messages
@@ -10,25 +19,50 @@ const customFormat = printf(({ level, message, label, timestamp }) => {
   return `${timestamp} [${label}] ${level}: ${message}`
 })
 
+/**
+ * Configuration options for the Logger.
+ *
+ * @interface LoggerConfig
+ * @property {string} [logDirectory] - The directory where log files will be stored.
+ * @property {string} [fileName] - The name of the log file.
+ * @property {any[]} [additionalTransports] - An array of additional transport mechanisms for logging.
+ */
 interface LoggerConfig {
   logDirectory?: string
   fileName?: string
   additionalTransports?: any[]
 }
 
+/**
+ * Default configuration object for the logger.
+ *
+ * @property {string} logDirectory - The directory where log files will be stored.
+ *                                   It is set to a 'logs' folder in the current working directory.
+ * @property {string} fileName - The name of the log file, which includes the application name and version.
+ */
 const defaultConfig = {
   logDirectory: path.join(process.cwd(), 'logs'),
   fileName: `${appName}-${appVersion}.log`
 }
 
+/**
+ * A singleton class that provides a logger instance.
+ * The logger instance is created with a default configuration and can be customized
+ * with additional configuration options.
+ */
 class LoggerSingleton {
-  private static instance: Logger;
+  private static instance: Logger
 
   private constructor() {}
 
+  /**
+   * Returns the singleton logger instance, creating it if it doesn't exist.
+   * @param customConfig Custom configuration options for the logger.
+   * @returns The logger instance.
+   */
   public static getInstance(customConfig: LoggerConfig = {}): Logger {
     if (!LoggerSingleton.instance) {
-      const config = { ...defaultConfig, ...customConfig };
+      const config = { ...defaultConfig, ...customConfig }
 
       const defaultTransports = [
         new transports.Console(),
@@ -36,19 +70,47 @@ class LoggerSingleton {
           filename: config.fileName,
           dirname: config.logDirectory
         })
-      ];
+      ]
 
       LoggerSingleton.instance = createLogger({
         format: combine(label({ label: appName }), timestamp(), customFormat),
         transports: [...defaultTransports, ...(customConfig.additionalTransports || [])]
-      });
+      })
     }
 
-    return LoggerSingleton.instance;
+    return LoggerSingleton.instance
   }
 }
 
-const logger = LoggerSingleton.getInstance();
+/**
+ * A singleton instance of the LoggerSingleton class.
+ * This instance is used to log messages throughout the application.
+ */
+const logger = LoggerSingleton.getInstance()
 
-export default logger;
-export { LoggerSingleton as createCustomLogger };
+/**
+ * Exports the logger instance.
+ *
+ * @remarks
+ * This allows the logger instance to be imported and used in other files.
+ *
+ * @example
+ * ```typescript
+ * import logger from 'path/to/logger';
+ * logger.info('This is an info message');
+ * ```
+ */
+export default logger
+
+/**
+ * Exports the LoggerSingleton as createCustomLogger.
+ *
+ * @remarks
+ * This allows the LoggerSingleton to be imported using the alias createCustomLogger.
+ *
+ * @example
+ * ```typescript
+ * import { createCustomLogger } from 'path/to/logger';
+ * const logger = createCustomLogger();
+ * ```
+ */
--- src/versions.ts
+++ src/versions.ts
@@ -1,11 +1,25 @@
 import * as path from 'path'
 import * as fs from 'fs'
 
+/**
+ * Represents the structure of a package.json file.
+ */
 interface PackageJson {
   name: string
   version: string
 }
 
+/**
+ * Retrieves the package.json file of the main application and returns its name and version.
+ *
+ * @returns {PackageJson} An object containing the name and version of the application.
+ *
+ * @remarks
+ * This function reads the package.json file located in the current working directory.
+ * If the name or version fields are not present in the package.json, it defaults to 'unknown-application' and '0.0.0' respectively.
+ *
+ * @throws {Error} If the package.json file cannot be read or parsed.
+ */
 function getPackageJson(): PackageJson {
   // Adjust the path to point to the main application's package.json
   const packageJsonPath = path.join(process.cwd(), 'package.json')
@@ -16,6 +30,18 @@ function getPackageJson(): PackageJson {
   }
 }
 
+/**
+ * Extracts the `name` and `version` properties from the package.json file.
+ *
+ * @returns An object containing the `name` and `version` of the package.
+ */
 const { name, version } = getPackageJson()
 
+/**
+ * Exports the application name and version.
+ *
+ * @module versions
+ * @exports appName - The name of the application.
+ * @exports appVersion - The version of the application.
+ */
 export { name as appName, version as appVersion }
